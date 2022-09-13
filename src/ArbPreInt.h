e9f84986d69998e95e92a9eef10f43f1268045e4
Frederick Watkins <freddiewatkins6@gmail.com>
2022-09-13 20:52:00 +0100

    Prototyped ArbPreInt functions

--- src/ArbPreInt.h
+++ src/ArbPreInt.h
@@ -1,3 +1,31 @@
 #pragma once
 
 #include "DynamicArray.h"
+
+typedef struct ArbPreInt {
+	DynamicArray arr;
+} ArbPreInt;
+
+// Initialize x
+void ArbPreInt_init(ArbPreInt* x);
+
+// Free all memory allocated to x
+void ArbPreInt_free(ArbPreInt* x);
+
+// Return x < y
+bool ArbPreInt_lessThan(ArbPreInt x, ArbPreInt y);
+
+// Return x > y
+bool ArbPreInt_moreThan(ArbPreInt x, ArbPreInt y);
+
+// Return x + y
+ArbPreInt ArbPreInt_add(ArbPreInt x, ArbPreInt y);
+
+// Return x - y
+ArbPreInt ArbPreInt_sub(ArbPreInt x, ArbPreInt y);
+
+// Return x * y
+ArbPreInt ArbPreInt_mul(ArbPreInt x, ArbPreInt y);
+
+// Reutrn x / y
+ArbPreInt ArbPreInt_div(ArbPreInt x, ArbPreInt y);
